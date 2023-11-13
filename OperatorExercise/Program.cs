f520808f98a6dd8f8187603534fee5c72ca95957
Josh Bostick <jbostick99@gmail.com>
2023-11-13 13:02:17 -0600

    Finished Exercises

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int dif = a - b;
+            int mul = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} with remainder {remainder}");
+
+            Console.WriteLine("Enter the radius of your circle");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of your circle is {AreaOfCircle(radius)}");
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * radius * radius;           
+            return area;    
         }
     }
 }
